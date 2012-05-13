83a40c9cf8114c66b76113e4b6d16e7a5ae3d38d
Chris Horrocks <chrisjhorrocks@gmail.com>
2012-05-13 17:15:30 +0100

    Updated from DTOP

--- Scripts/HA_Monitor.py
+++ Scripts/HA_Monitor.py
@@ -19,22 +19,31 @@ def Scheduleimport():
         except:
                 print "Error: Could not run Schedule Importer script.";
         print;
-        x = raw_input("Press Enter to Return to Menu");   
+		# Pause
+		x = raw_input("Press Enter to Return to Menu");   
          
 def ScheduleEdit():
-    os.system('nano /home/pi/.homeautomation/Schedule/schedule');
+    # Edit the master schedule with nano
+	os.system('nano /home/pi/.homeautomation/Schedule/schedule');
+	# Recreate the days schedule
     Scheduleimport();
     
 def SensorCheck(Currenttime,Temprate):
-    sensorfile = open('/home/pi/.homeautomation/Sensors/1', 'r');
-    sensordata = sensorfile.read();
-    split = shlex.shlex(sensordata);
-    split.whitespace += ',';
-    split.whitespace_split = True;
-    sensorstats = list(split);
-    Sensortype = sensorstats[0];
-    Sensortemp = sensorstats[1];
-    
+    try:
+		# Open the first sensors data file
+		sensorfile = open('/home/pi/.homeautomation/Sensors/1', 'r');
+		sensordata = sensorfile.read();
+		# Create a list from the csv data
+		split = shlex.shlex(sensordata);
+		split.whitespace += ',';
+		split.whitespace_split = True;
+		sensorstats = list(split);
+		# Populate the Sensor variables from the list
+		Sensortype = sensorstats[0];
+		Sensortemp = sensorstats[1];
+    except:
+		print "Error: Could not get sensor stats.";
+	# Kick-off the next function
     Processing(Currenttime,Sensortemp,Sensortype,Temprate);
 
 def Processing(Currenttime,Sensortemp,Sensortype,Temprate):
@@ -57,14 +66,14 @@ def Processing(Currenttime,Sensortemp,Sensortype,Temprate):
     # Calculate heating time req
     Temptimereq = Tempdiff / Temprate;
 
+	# Check the sensor type and correct accordingly
     if Sensortype == "v":
         Sensortype = "Virtual Sensor";
     elif Sensortype == "p":
         Sensortype = "Physical Sensor";
-
+	
+	# Kick-off the next function
     Formatting(Currenttime,Schedtemp,Sensortemp,Sensortype,Tempdiff,Temptimereq);
-
-    
 
 def Formatting(Currenttime,Schedtemp,Sensortemp,Sensortype,Tempdiff,Temptimereq):
     # Format time string
@@ -87,11 +96,12 @@ def Formatting(Currenttime,Schedtemp,Sensortemp,Sensortype,Tempdiff,Temptimereq)
     strTemptimereq = str(Temptimereq);
     strTemptimereq += " mins";
 
+	# Kick-off the next function
     Display(strCurrenttime,strSchedtemp,strSensortemp,Sensortemp,Sensortype,strTempdiff,Tempdiff,strTemptimereq);
  
-
 def Display(strCurrenttime,strSchedtemp,strSensortemp,Sensortemp,Sensortype,strTempdiff,Tempdiff,strTemptimereq):
-    print "                     System Monitor";
+    # Print system status
+	print "                     System Monitor";
     print;
     print "##############";
     print "#  Overview  #";
@@ -107,11 +117,13 @@ def Display(strCurrenttime,strSchedtemp,strSensortemp,Sensortemp,Sensortype,strT
     print "The temperature at", strCurrenttime, "should be", strSchedtemp;
     print "The temperature is currently:", strSensortemp;
     print "This is a difference of:", strTempdiff;
-    if Tempdiff > 0:
+    # Check whether heater or fan is applicable
+	if Tempdiff > 0:
         print "The heater will be activated for", strTemptimereq;
     else:
         print "The fan will be activated for", strTemptimereq[1:]
     print;
+	# Pause
     x = raw_input("Press Enter to Return to Menu");
 
 def Menu():
@@ -126,22 +138,32 @@ def Menu():
     print "3) Edit Schedule";
     print "4) Exit";
     print;
+	# Input prompt
     return input ("Option: ");
 
-# Start
+# Start app
 loop = 1
 choice = 0
+# Start main menu and wait for option
 while loop == 1:
     choice = Menu();
     if choice == 1:
+		# Clear screen
         os.system('clear');
+		# Start monitor cycle
         SensorCheck(Currenttime,Temprate);
     elif choice == 2:
+		# Clear screen
         os.system('clear');
+		# Recreate todays schedule
         Scheduleimport();
     elif choice == 3:
+		# Clear screen
         os.system('clear');
+		# Edit the master schedule
         ScheduleEdit();
     elif choice == 4:
+		# Clear screen
         os.system('clear');
+		# Break from loop (exit)
         break;
